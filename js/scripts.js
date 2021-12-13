403baf3b5eabe1ffb417eea098f89c753359cfdc
efarnan <75091127+efarnan@users.noreply.github.com>
2021-12-13 15:33:17 +0100

    Update to add features & fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let combosFound = 0;  
+let movesCounter = 0
+let count = 60;
 
 function flipCard() {
   if (lockBoard) return;
@@ -10,32 +13,42 @@ function flipCard() {
 
   this.classList.add('flip');
 
+  movesCounter++;
+  //MOVE COUNTER{  
+  document.getElementById('moves').innerHTML= movesCounter;
+
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
 
     return;
   }
-
+  
   secondCard = this;
 
   checkForMatch();
 }
+//leave flipCard as current function format. OR Pass in a variable to the button to use new synax
 
-function checkForMatch() {
+const checkForMatch = () => {
    if (firstCard.dataset.framework === secondCard.dataset.framework) {
          disableCards();
+         combosFound++;
          return;
+
+         if (combosFound === 8){
+           victory();
+         }
     }
         
     unflipCards();
-     }
+}
 
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
-/*me */
+
     setTimeout(() => {
         firstCard.style.visibility = 'hidden';
         secondCard.style.visibility = 'hidden';
@@ -69,3 +82,83 @@ function resetBoard() {
  })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+//TUTORAL MODAL 
+const modal = document.getElementById("tutModal");
+// Get the button that opens the modal
+const btn = document.getElementById("tut-btn");
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal
+btn.onclick = function modalOpen() {
+  modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function modalClose() {
+  modal.style.display = "none";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function modalExit(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+ }
+}
+
+
+
+//START GAME
+const startGame = () => {
+    //TIMER
+  let interval = setInterval( () => {
+    document.getElementById('time-remaining').innerHTML= count;
+    count--;
+
+    if (count === 0){
+      clearInterval(interval);
+      document.getElementById('time-remaining').innerHTML='Done';
+
+      gameOver();
+  }
+}, 1000);
+}
+
+
+const reStartGame = () => {
+  startGame();
+
+//Restart Confirmation Modal
+  const modal = document.getElementById('rsrt-modal');
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      }
+  } 
+}
+
+
+//GAME OVER
+function gameOver(){
+  clearInterval(interval);
+  document.getElementById('game-over-text').classList.add('visible')
+}
+
+//VICTORY
+function victory(){
+  clearInterval(interval);
+  documemt.getElementById('victory-text').classList.add('visible');
+}
+
+function ready(){
+  const overlays = Array.from(document.getElementsByClassName('overlay-text'));
+  const cards = Array.from(document.getElementsByClassName('memory-card'));
+
+  overlays.forEach(overlay => { 
+   overlay.addEventListener('click', () => {
+    overlay.classList.remove('visible');
+    startGame();
+    console.log ('test')
+  })
+});
+}
+ready();
